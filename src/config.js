4be145ee6c47b657d175572f4a1167c97c0c9ce5
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2020-01-20 13:54:22 -0500

    cloned for testing purposes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://blooming-mesa-14506.herokuapp.com/api',
+    API_ENDPOINT: 'https://tonewoods-thinkful-test.herokuapp.com/api',
     // API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: process.env.REACT_APP_API_KEY,
   }
