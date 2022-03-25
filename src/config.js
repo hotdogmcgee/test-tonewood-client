6ea66551898a80cad7876f634af405c9e64cf5cb
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2022-03-25 13:42:59 -0700

    update api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://test-tonewood-api.herokuapp.com/api',
+    API_ENDPOINT: 'https://stormy-depths-24327.herokuapp.com/api/',
     // API_ENDPOINT: 'http://localhost:8000/api',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
